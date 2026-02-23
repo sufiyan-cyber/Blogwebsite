d1b3ca09ff7ef8852d07963877f35a67a2192684
sufiyan-cyber <sufiyankhan321123@gmail.com>
2026-02-23 16:28:03 +0530

    initial commit with unverisoned files

---
+++ forms.py
@@ -0,0 +1,36 @@
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField,PasswordField
+from wtforms.validators import DataRequired, URL, Length
+from flask_ckeditor import CKEditorField
+
+
+# WTForm for creating a blog post
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+# TODO: Create a RegisterForm to register new users
+class Createregistrationform(FlaskForm):
+    email=StringField("your email",validators=[DataRequired()])
+    password=PasswordField("your password",validators=[DataRequired()])
+    name=StringField("your name",validators=[DataRequired()])
+    submit=SubmitField("submit form")
+
+
+# TODO: Create a LoginForm to login existing users
+
+class Loginform(FlaskForm):
+    email=StringField("your email",validators=[DataRequired()])
+    password=PasswordField("your password",validators=[DataRequired()])
+    submit=SubmitField("submit form")
+
+
+# CommentForm for users to leave comments below posts
+class CommentForm(FlaskForm):
+    comment_text = CKEditorField("Comment", validators=[DataRequired(), Length(max=500)])
+    submit = SubmitField("Submit Comment")
